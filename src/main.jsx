9b02f1092182ea131c25af30de5048395954ccfa
arazdev <hello@example.com>
2024-07-19 00:54:36 -0500

    setup gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux'
 import App from './App.jsx'
 import store from './store.js'
 import './index.css'
-import { HashRouter as Router } from "react-router-dom";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
