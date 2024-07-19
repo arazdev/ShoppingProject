19b4b9f0419f26122e5fee5a240c3f594af1e6ad
arazdev <hello@example.com>
2024-07-19 00:11:47 -0500

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Header from './Components/Header';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/ShoppingProject">
       <Header />
       <Routes>
         <Route path='/' element={<LandingPage />} />
