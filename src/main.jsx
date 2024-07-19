a177c6f9820efc1582bef744a140cb7c9666202b
arazdev <hello@example.com>
2024-07-19 00:44:02 -0500

    setup gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import { Provider } from 'react-redux'
 import App from './App.jsx'
 import store from './store.js'
 import './index.css'
+import { HashRouter as Router } from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
 )
